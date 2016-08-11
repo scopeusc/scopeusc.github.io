223dd1dc7259d44ebcd8e8bec3ac20498a75c41f
Alex Pareto <alex.pareto@gmail.com>
2016-08-11 11:10:28 -0400

    updating descriptions and apply button

--- main.js
+++ main.js
@@ -12,10 +12,34 @@ $('#learn-more-button').click(function() {
     $('html, body').animate({scrollTop: $(".about").offset().top}, 1000);
 })
 
+$('.page-apply').click(function() {
+    $('html, body').animate({scrollTop: $(".typeform-widget").offset().top}, 1000);
+})
+
+$('#apply-button').click(function() {
+    $('html, body').animate({scrollTop: $(".typeform-widget").offset().top}, 1000);
+})
+
+/* @deprecated for now
 function hover(element) {
     element.setAttribute('src', 'assets/logo-red.png');
 }
 function unhover(element) {
     element.setAttribute('src', 'assets/logo-purp.png');
 }
+*/
+
+<!--//--><![CDATA[//><!--
+var images = new Array()
+function preload() {
+    for (i = 0; i < preload.arguments.length; i++) {
+        images[i] = new Image()
+        images[i].src = preload.arguments[i]
+    }
+}
+preload(
+    "assets/logo-red.png",
+    "assets/logo-purp.png"
+)
+//--><!]]>
 

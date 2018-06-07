54755773b8f5487eec1e33bbdbab4be523c4df91
Yun Tian Xu <yuntonyx@gmail.com>
2018-06-07 11:30:04 +0800

    Add smooth scroll

--- custom_assets/js/main.js
+++ custom_assets/js/main.js
@@ -1,8 +1,16 @@
-// function to handle text typing animation
 $(function() {
+    // function to handle text typing animation
     $('#animatetext').typed({
         strings: ["Learn new things.", "Build cool projects.", "Join an awesome community.", "Impress interviewers.", "Learn. Build. Code."],
         typeSpeed: 10,
         callback: function() {}
+    });
+
+    // smooth scroll
+    $('[data-smooth-scroll="true"]').click(function() {
+        var href = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, 1000);
     });
 });

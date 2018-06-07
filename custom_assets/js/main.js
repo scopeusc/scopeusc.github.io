76caf02131fbe992947f4cfc71e7fce9c6bb4b7d
Aliya Petranik <petranik@usc.edu>
2018-06-06 17:55:59 -0700

    added social links

---
+++ custom_assets/js/main.js
@@ -0,0 +1,8 @@
+// function to handle text typing animation
+$(function() {
+    $('#animatetext').typed({
+        strings: ["Learn new things.", "Build cool projects.", "Join an awesome community.", "Impress interviewers.", "Learn. Build. Code."],
+        typeSpeed: 10,
+        callback: function() {}
+    });
+});
